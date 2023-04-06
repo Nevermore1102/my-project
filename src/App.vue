8cc5961d5c35b3cb637a912f8563f80e20a7d810
zh <1457308076@qq.com>
2023-04-06 20:01:56 +0800

    修改后

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view></router-view> <!-- 添加这一行 -->
+  </div>
 </template>
 
 <script>
